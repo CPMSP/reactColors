d74f98b00c0252a98dc348a5faaff459f8a4064d
CPMSP <cpetersonmsp@gmail.com>
2020-04-13 15:21:04 -0500

    Add SingleColorPalette and Link

--- src/ColorBox.jsx
+++ src/ColorBox.jsx
@@ -15,7 +15,7 @@ export default class ColorBox extends Component {
 		});
 	}
 	render() {
-		const { name, background } = this.props;
+		const { name, background, moreUrl } = this.props;
 		const { copied } = this.state;
 		return (
 			<CopyToClipboard text={background} onCopy={this.changeCopyState}>
@@ -34,11 +34,9 @@ export default class ColorBox extends Component {
 						</div>
 						<button className="copy-button">Copy</button>
 					</div>
-					{this.props.showingAllColors && (
-						<Link to="/" onClick={e => e.stopPropagation()}>
-							<span className="see-more">More</span>
-						</Link>
-					)}
+					<Link to={moreUrl} onClick={e => e.stopPropagation()}>
+						<span className="see-more">More</span>
+					</Link>
 				</div>
 			</CopyToClipboard>
 		);
