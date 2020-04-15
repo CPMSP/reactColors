20deaf654b6d18c8b134d1c4de406765db317df8
CPMSP <cpetersonmsp@gmail.com>
2020-04-15 15:15:19 -0500

    Add Navbar Styles and remove CSS files

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,10 +6,11 @@ import Snackbar from "@material-ui/core/Snackbar";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import Slider from "rc-slider";
+import { withStyles } from "@material-ui/styles";
 import "rc-slider/assets/index.css";
-import "./Navbar.css";
+import styles from "./styles/NavbarStyles";
 
-export default class Navbar extends Component {
+class Navbar extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { format: "hex", open: false };
@@ -27,17 +28,17 @@ export default class Navbar extends Component {
 	}
 
 	render() {
-		const { level, changeLevel, showingAllColors } = this.props;
+		const { level, changeLevel, showingAllColors, classes } = this.props;
 		const { format } = this.state;
 		return (
-			<header className="Navbar">
-				<div className="logo">
+			<header className={classes.Navbar}>
+				<div className={classes.logo}>
 					<Link to="/">reactcolorpicker</Link>
 				</div>
 				{showingAllColors && (
-					<div className="slider-container">
+					<div>
 						<span>Level: {level}</span>
-						<div className="slider">
+						<div className={classes.slider}>
 							<Slider
 								defaultValue={level}
 								min={100}
@@ -48,7 +49,7 @@ export default class Navbar extends Component {
 						</div>
 					</div>
 				)}
-				<div className="select-container">
+				<div className={classes.selectContainer}>
 					<Select value={format} onChange={this.handleFormatChange}>
 						<MenuItem value="hex">HEX - #ffffff</MenuItem>
 						<MenuItem value="rgb">
@@ -84,3 +85,5 @@ export default class Navbar extends Component {
 		);
 	}
 }
+
+export default withStyles(styles)(Navbar);
