a46f6408e7a92690930523494ac6bec5dd9e212a
CPMSP <cpetersonmsp@gmail.com>
2021-01-12 00:00:02 -0600

    Updates

---
+++ precache-manifest.133870bc3af86f6a1c36be6c3d7d7256.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a21ae1f09056e7e9afa0838147fcac",
+    "url": "/reactColors/index.html"
+  },
+  {
+    "revision": "6a02cc2f7a35b6c5ecf9",
+    "url": "/reactColors/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "ad1a8b147f29d67a6b1c",
+    "url": "/reactColors/static/css/main.62ee0c85.chunk.css"
+  },
+  {
+    "revision": "6a02cc2f7a35b6c5ecf9",
+    "url": "/reactColors/static/js/2.1cf535c3.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/reactColors/static/js/2.1cf535c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad1a8b147f29d67a6b1c",
+    "url": "/reactColors/static/js/main.8e676c1c.chunk.js"
+  },
+  {
+    "revision": "8eb6d229e1be1c0980f1",
+    "url": "/reactColors/static/js/runtime-main.7f8f2029.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/reactColors/static/media/bg.77428cd4.svg"
+  }
+]);
